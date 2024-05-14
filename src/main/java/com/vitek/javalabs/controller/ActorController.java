eed7a8063cb238985e2465b91d6d7516014f195c
0rpheus-0 <erofeevvictor25@gmail.com>
2024-05-14 16:27:14 +0300

    cross origin

--- src/main/java/com/vitek/javalabs/controller/ActorController.java
+++ src/main/java/com/vitek/javalabs/controller/ActorController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.vitek.javalabs.dto.ActorDto;
 import com.vitek.javalabs.service.ActorService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/actor")
 public class ActorController {
--- src/main/java/com/vitek/javalabs/controller/MovieController.java
+++ src/main/java/com/vitek/javalabs/controller/MovieController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.vitek.javalabs.aop.RequestStats;
 import com.vitek.javalabs.dto.MovieDto;
 import com.vitek.javalabs.service.MovieService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/movie")
 @RequestStats
